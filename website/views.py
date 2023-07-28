f1af92a1dad7458a698b76a6d3f6de7aa67640d2
Mans-Alien <mans012033945420@gmail.com>
2023-07-28 16:20:06 +0300

    add the css and js files of the bootstratp and edite the navbar

--- website/views.py
+++ website/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {})
+    #check to see if logging in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #authenticate
+        user = authenticate(request, username=username, password= password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Succesfully logged in")
+            return redirect("home")
+
+        else:
+            messages.success(request, "error, please make sure of the username and password and try again")
+            return redirect("home")
+    else:
+        return render(request, "home.html", {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "logout...")
+    return redirect("home")
